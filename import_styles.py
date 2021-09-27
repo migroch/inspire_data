38e8ab3655178d231c8f0849b1f1ee1c23f0e98c
Miguel Rocha <migroch@gmail.com>
2021-09-27 16:59:41 -0700

    Added new files

---
+++ import_styles.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+
+def remote_css(url):
+    st.markdown('<style src="{}"></style>'.format(url), unsafe_allow_html=True)
+
+def icon_css(icone_name):
+    remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+
+def icon(icon_name):
+    st.markdown('<i class="material-icons">{}</i>'.format(icon_name), unsafe_allow_html=True)
+
+def import_bootstrap():
+    local_css("styles/bootstrap.min.css")
+    st.markdown('''
+    <script src="styles/bootstrap.bundle.min.js"></script>
+    ''', unsafe_allow_html=True)
+
+import_bootstrap()
+local_css('styles/main.css')
