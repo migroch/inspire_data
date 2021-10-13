82bf0676ee46dbcf46394fb82802a637843d13ca
Miguel Rocha <migroch@gmail.com>
2021-10-13 15:55:24 -0700

    time_chart rebuilt

---
+++ time_chart/frontend/build/precache-manifest.73278e27c45f07e8a894a85b1c471267.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3442aa593edd2a4e6e7e8e89d7a4f6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7c8f1ed8f617b3d4b949",
+    "url": "./static/css/main.ddba637b.chunk.css"
+  },
+  {
+    "revision": "ed7e21c9212864494e37",
+    "url": "./static/js/2.30cc49b6.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.30cc49b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c8f1ed8f617b3d4b949",
+    "url": "./static/js/main.12281d3b.chunk.js"
+  },
+  {
+    "revision": "8e47d6cf5771f2854f25",
+    "url": "./static/js/runtime-main.5d38f8c8.js"
+  }
+]);
