96754727c88398b18ebe4e14b71ff696127c4776
Miguel Rocha <migroch@gmail.com>
2021-11-01 15:01:10 -0700

    Added build directories

---
+++ gauge_chart/frontend/build/precache-manifest.1cbf9c772ec63c1d37b8d5429ae00f44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e677827269d33f3ba103f2d976839a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fee1ac3991c7bbf59434",
+    "url": "./static/css/main.0304cd6a.chunk.css"
+  },
+  {
+    "revision": "0cd8e0937e138576c7e0",
+    "url": "./static/js/2.c5b2b5d6.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.c5b2b5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee1ac3991c7bbf59434",
+    "url": "./static/js/main.56115ea3.chunk.js"
+  },
+  {
+    "revision": "8e47d6cf5771f2854f25",
+    "url": "./static/js/runtime-main.5d38f8c8.js"
+  }
+]);
