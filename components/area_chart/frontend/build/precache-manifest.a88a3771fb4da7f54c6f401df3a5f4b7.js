2f5d931ec6930a40f20ca73e53b408429e9e112f
Miguel Rocha <migroch@gmail.com>
2022-01-06 20:16:56 -0800

    Re-buit area chart

---
+++ components/area_chart/frontend/build/precache-manifest.a88a3771fb4da7f54c6f401df3a5f4b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2108d4904f69171e1dc39a34248c1b04",
+    "url": "./index.html"
+  },
+  {
+    "revision": "711d69753f574ade827f",
+    "url": "./static/css/main.c1c17c26.chunk.css"
+  },
+  {
+    "revision": "8d95f0c5f3250280bedf",
+    "url": "./static/js/2.a5632f94.chunk.js"
+  },
+  {
+    "revision": "56694876f31beacb8d99137e09ff172a",
+    "url": "./static/js/2.a5632f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "711d69753f574ade827f",
+    "url": "./static/js/main.c754f23f.chunk.js"
+  },
+  {
+    "revision": "8c5c9d6f741ab8d19a27",
+    "url": "./static/js/runtime-main.13ebc483.js"
+  }
+]);
